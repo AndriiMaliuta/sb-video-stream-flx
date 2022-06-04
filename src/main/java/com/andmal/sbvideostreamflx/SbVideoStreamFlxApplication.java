7a0f5b74566c4ca8e7d055ef05272cddcc1dbec5
andrii maliuta <quadr988"gmail.com>
2022-06-04 19:43:31 +0300

    init

---
+++ src/main/java/com/andmal/sbvideostreamflx/SbVideoStreamFlxApplication.java
@@ -0,0 +1,13 @@
+package com.andmal.sbvideostreamflx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbVideoStreamFlxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbVideoStreamFlxApplication.class, args);
+    }
+
+}
